97e702d1502b077b2cb0cc657f2fd7f13134bd48
HtetAungShine6 <htetaungshine211299@gmail.com>
2025-08-03 16:04:22 +0700

    Initial Commit

---
+++ Event/EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventApp.swift
+//  Event
+//
+//  Created by Htet Aung Shine on 3/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
